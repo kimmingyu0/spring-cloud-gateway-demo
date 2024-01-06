d9bb39e8ad0f62ad55dcfe3ea24d3136e5699440
kimmingyu0 <minku4820@gmail.com>
2024-01-06 20:36:52 +0900

    Feat : Gateway Custom filter Bean으로 등록

--- spring-cloud-gateway-server/src/main/java/com/gatewayserver/FilterConfig.java
+++ spring-cloud-gateway-server/src/main/java/com/gatewayserver/FilterConfig.java
@@ -1,24 +1,27 @@
 package com.gatewayserver;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig {
+
+    private final CustomFilter customFilter;
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 //first-micro-service
                 .route(r -> r.path("/first-micro-service/**")
-                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
-                                .addResponseHeader("first-response", "first-response-header"))
+                        .filters(f -> f.filter(customFilter.apply(customFilter.newConfig())))
                         .uri("http://localhost:8081"))
                 //second-micro-service
                 .route(r -> r.path("/second-micro-service/**")
-                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
-                                .addResponseHeader("second-response", "second-response-header"))
+                        .filters(f -> f.filter(customFilter.apply(customFilter.newConfig())))
                         .uri("http://localhost:8082"))
                 .build();
     }
