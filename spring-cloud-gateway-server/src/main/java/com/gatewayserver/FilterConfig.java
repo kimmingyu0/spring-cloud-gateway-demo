60921af2e8dde6630e075132727d7fc8c404af0e
kimmingyu0 <minku4820@gmail.com>
2024-01-06 20:02:32 +0900

    Feat : Fix Spring Cloud Gateway configuration from Yaml to Java code

---
+++ spring-cloud-gateway-server/src/main/java/com/gatewayserver/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.gatewayserver;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //first-micro-service
+                .route(r -> r.path("/first-micro-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:8081"))
+                //second-micro-service
+                .route(r -> r.path("/second-micro-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
